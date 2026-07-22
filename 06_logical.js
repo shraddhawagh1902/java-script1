9b55669cf09fa690ed10a10f5b7697d2a141fa57
Wagh Shraddha <shraddhaw1902@gmail.com>
2026-07-22 07:11:46 +0000

    logical.js

---
+++ 06_logical.js
@@ -0,0 +1,12 @@
+let a = 567;
+let b = 123;
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+
+let age = 25;
+let isAdult = true;
+
+console.log(age >= 18 && isAdult);
+console.log(age < 18 || isAdult);
+console.log(!isAdult);
